ec9b6301469bdd413ff9de8f0daadc275c39800c
PK <59316816+rprvnkmr4@users.noreply.github.com>
2021-11-02 00:20:31 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "access_key" {
+  default = "AKIA3ODFABMH54TMS3W2"
+}
+
+variable "secret_key" {
+  default = "UBPTFlYk0fvn92nR1udJAP887jgFUN6QO60uwHju"
+}
+
